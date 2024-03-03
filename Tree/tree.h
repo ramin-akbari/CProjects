607d8453e614252d6506962ecd6ab497baa4a5fa
Ramin Akbari <akbarigh@msu.edu>
2024-03-03 17:07:49 -0500

    added functions

--- Tree/tree.h
+++ Tree/tree.h
@@ -1,3 +1,14 @@
 typedef struct tree {
-
+    int data;
+    struct tree *left;
+    struct tree *right;
+    struct tree *parent;    
+    void (*add_right) (tree*,int);
+    void (*add_left) (tree*,int);
+    void (*remove) (tree*);
 } tree;
+
+tree* init_tree();
+void add_to_left(tree*,int);
+void add_to_right(tree*, int);
+void delete_node(tree*);
